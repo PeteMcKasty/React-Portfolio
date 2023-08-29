f03acdd7c05857ab95e88ad1f717d75ed7a33920
Peter McKasty <pmck25@yahoo.com>
2023-08-29 18:10:27 -0400

    added contact framework using react in contact.js

---
+++ components/contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <section>
+      <h2>Contact</h2>
+      <h3>Github</h3>
+      <h3>pmck25@yahoo.com</h3>
+      <h3>LinkedIn</h3>
+    </section>
+  );
+};
+
+export default Contact;
