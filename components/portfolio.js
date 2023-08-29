8aa3a8244322025e0a457007706ba30ddd6fa960
Peter McKasty <pmck25@yahoo.com>
2023-08-28 21:23:40 -0400

    created the react framework for the actual portfolio portin of the application

---
+++ components/portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Portfolio = () => {
+  return (
+    <section>
+      <h2>Portfolio</h2>
+      {/* Will insert imagery and links to projects */}
+    </section>
+  );
+};
+
+export default Portfolio;
