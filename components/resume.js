a556275725982e2e718825f34bd85354c0cd0566
Peter McKasty <pmck25@yahoo.com>
2023-08-29 18:43:53 -0400

    added resume element programatically using react in resume.js

---
+++ components/resume.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Resume = () => {
+  return (
+    <section>
+      <h2>Resume</h2>
+      {/* will develop further to have a link to my resume */}
+    </section>
+  );
+};
+
+export default Resume;
