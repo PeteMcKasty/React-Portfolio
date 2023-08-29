05acc75be8baecd393477adc574b504e722409da
Peter McKasty <pmck25@yahoo.com>
2023-08-28 20:41:24 -0400

    added about me framework. about me content still needs to be added

---
+++ components/aboutMe.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutMe = () => {
+  return (
+    <section>
+      <h2>About Me</h2>
+      {/* I will eventually add the about me and my photo here */}
+    </section>
+  );
+};
+
+export default AboutMe;
